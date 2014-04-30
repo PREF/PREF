7256a0b9e90629b01ee08be1e560f470309ca27c
Dax <trogu.davide@gmail.com>
2014-04-30 11:53:24 +0200

    PrefUI: More API Implementation

--- prefsdk/api/prefuiapi.cpp
+++ prefsdk/api/prefuiapi.cpp
@@ -4,45 +4,85 @@ namespace PrefSDK
 {
     namespace API
     {
-        TableWidget *PrefUI_createTableWidget(const char *title)
+        void Widget_resize(QWidget *__this, int w, int h)
         {
-            TableWidget* tablewidget = new TableWidget();
-            tablewidget->setWindowTitle(QString::fromUtf8(title));
-            return tablewidget;
+            __this->resize(w, h);
         }
 
-        void TableWidget_setColumnCount(TableWidget *__this, int count)
+        QLayout *Layout_createHorizontal()
         {
-            __this->table()->setColumnCount(count);
+            return new QHBoxLayout();
         }
 
-        void TableWidget_setRowCount(TableWidget *__this, int count)
+        QLayout *Layout_createVertical()
         {
-            __this->table()->setRowCount(count);
+            return new QVBoxLayout();
         }
 
-        void TableWidget_setHeaderItem(TableWidget *__this, int column, const char *text)
+        void BoxLayout_addLayout(QBoxLayout *__this, QLayout *layout, bool stretch)
         {
-            __this->table()->setHorizontalHeaderItem(column, new QTableWidgetItem(text));
-            __this->table()->resizeColumnsToContents();
-            __this->table()->resizeRowsToContents();
+            __this->addLayout(layout, stretch);
         }
 
-        void TableWidget_setItem(TableWidget *__this, int row, int column, const char *text)
+        void Layout_addWidget(QLayout *__this, QWidget *widget)
         {
-            __this->table()->setItem(row, column, new QTableWidgetItem(text));
-            __this->table()->resizeColumnsToContents();
-            __this->table()->resizeRowsToContents();
+            __this->addWidget(widget);
         }
 
-        void PrefUI_show(QDialog *__this)
+        void Layout_setMargins(QLayout *__this, int left, int top, int right, int bottom)
+        {
+            __this->setContentsMargins(left, top, right, bottom);
+        }
+
+        QDialog *Dialog_create(const char *title)
+        {
+            QDialog* dialog = new QDialog();
+            dialog->setWindowTitle(QString::fromUtf8(title));
+            return dialog;
+        }
+
+        void Dialog_setLayout(QDialog *__this, QLayout *layout)
+        {
+            __this->setLayout(layout);
+        }
+
+        void Dialog_show(QDialog *__this)
         {
             __this->show();
         }
 
-        void PrefUI_showModal(QDialog *__this)
+        void Dialog_exec(QDialog *__this)
         {
             __this->exec();
+        }
+
+        QTableWidget *TableWidget_create()
+        {
+            return new QTableWidget();
+        }
+
+        void TableWidget_setColumnCount(QTableWidget *__this, int count)
+        {
+            __this->setColumnCount(count);
+        }
+
+        void TableWidget_setRowCount(QTableWidget *__this, int count)
+        {
+            __this->setRowCount(count);
+        }
+
+        void TableWidget_setHeaderItem(QTableWidget *__this, int column, const char *text)
+        {
+            __this->setHorizontalHeaderItem(column, new QTableWidgetItem(text));
+            __this->resizeColumnsToContents();
+            __this->resizeRowsToContents();
+        }
+
+        void TableWidget_setItem(QTableWidget *__this, int row, int column, const char *text)
+        {
+            __this->setItem(row, column, new QTableWidgetItem(text));
+            __this->resizeColumnsToContents();
+            __this->resizeRowsToContents();
         }
     }
 }
--- prefsdk/api/prefuiapi.h
+++ prefsdk/api/prefuiapi.h
@@ -1,7 +1,9 @@
 #ifndef PREFSDK_API_H
 #define PREFSDK_API_H
 
-#include "prefsdk/ui/tablewidget.h"
+#include <QtCore>
+#include <QtGui>
+#include <QtWidgets>
 
 namespace PrefSDK
 {
@@ -9,16 +11,28 @@ namespace PrefSDK
     {
         extern "C"
         {
-            /* View API */
-            TableWidget* PrefUI_createTableWidget(const char* title);
-            void PrefUI_show(QDialog* __this);
-            void PrefUI_showModal(QDialog* __this);
+            /* QWidget API */
+            void Widget_resize(QWidget *__this, int w, int h);
 
-            /* Table Widget API */
-            void TableWidget_setColumnCount(TableWidget* __this, int count);
-            void TableWidget_setRowCount(TableWidget* __this, int count);
-            void TableWidget_setHeaderItem(TableWidget *__this, int column, const char* text);
-            void TableWidget_setItem(TableWidget *__this, int row, int column, const char* text);
+            /* QLayout API */
+            QLayout* Layout_createHorizontal();
+            QLayout* Layout_createVertical();
+            void BoxLayout_addLayout(QBoxLayout* __this, QLayout* layout, bool stretch);
+            void Layout_addWidget(QLayout* __this, QWidget* widget);
+            void Layout_setMargins(QLayout* __this, int left, int top, int right, int bottom);
+
+            /* QDialog API */
+            QDialog* Dialog_create(const char* title);
+            void Dialog_setLayout(QDialog* __this, QLayout* layout);
+            void Dialog_show(QDialog* __this);
+            void Dialog_exec(QDialog* __this);
+
+            /* QTableWidget API */
+            QTableWidget* TableWidget_create();
+            void TableWidget_setColumnCount(QTableWidget* __this, int count);
+            void TableWidget_setRowCount(QTableWidget* __this, int count);
+            void TableWidget_setHeaderItem(QTableWidget *__this, int column, const char* text);
+            void TableWidget_setItem(QTableWidget *__this, int row, int column, const char* text);
         }
     }
 }
