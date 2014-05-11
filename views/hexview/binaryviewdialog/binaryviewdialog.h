df5114c6d3bf4db04a87226e2f56d9a118401490
Dax <trogu.davide@gmail.com>
2014-05-11 15:07:54 +0200

    Implemented BinaryViewDialog

--- views/hexview/binaryviewdialog/binaryviewdialog.h
+++ views/hexview/binaryviewdialog/binaryviewdialog.h
@@ -1,7 +1,10 @@
 #ifndef BINARYVIEWDIALOG_H
 #define BINARYVIEWDIALOG_H
 
-#include <QDialog>
+#include <QtCore>
+#include <QtGui>
+#include <QtWidgets>
+#include "qhexedit/qhexeditdata.h"
 
 namespace Ui {
 class BinaryViewDialog;
@@ -11,12 +14,27 @@ class BinaryViewDialog : public QDialog
 {
     Q_OBJECT
 
-public:
-    explicit BinaryViewDialog(QWidget *parent = 0);
-    ~BinaryViewDialog();
+    public:
+        explicit BinaryViewDialog(QHexEditData *hexeditdata, QWidget *parent = 0);
+        ~BinaryViewDialog();
 
-private:
-    Ui::BinaryViewDialog *ui;
+    private slots:
+        void onCtrlModifier(bool pressed);
+        void onShiftModifier(bool pressed);
+
+    protected:
+        virtual void closeEvent(QCloseEvent* event);
+        virtual void keyPressEvent(QKeyEvent* event);
+        virtual void keyReleaseEvent(QKeyEvent* event);
+
+    signals:
+        void gotoTriggered(qint64);
+
+    private:
+        Ui::BinaryViewDialog *ui;
+        QHexEditData* _hexeditdata;
+        bool _ctrlpressed;
+        bool _shiftpressed;
 };
 
 #endif // BINARYVIEWDIALOG_H
