311539bf697a530ed04f4cb0b283cecc13f78b31
Dax <trogu.davide@gmail.com>
2014-11-15 13:07:34 +0100

    LuaOOP: Fixed Stack Overflow when calling "__super" method

--- prefsdk/libs/oop/oop.cpp
+++ prefsdk/libs/oop/oop.cpp
@@ -62,7 +62,9 @@ namespace PrefSDK
 
         if(!lua_isnoneornil(l, -1))
         {
-            for(int i = 1; i <= argc; i++)
+            lua_pushvalue(l, lua_upvalueindex(1)); /* Push Base Class' self */
+
+            for(int i = 2; i <= argc; i++)
                 lua_pushvalue(l, i);
 
             int err = lua_pcall(l, argc, 0, 0);
