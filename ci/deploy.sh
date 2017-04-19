32edecea28a9530e0f6bb9d310befcd6359b56d0
Dax89 <trogu.davide@gmail.com>
2017-04-19 15:45:57 +0200

    TravisCI: Added custom deploy step

---
+++ ci/deploy.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+
+DEPLOY_DIR=deploy
+OS_NAME=`uname`
+ARCH=`uname -m`
+BUILD_DATE=`date +%d%m%Y`
+BUILD_ID="PREF_"$OS_NAME"_"$ARCH"_"$BUILD_DATE
+BUILD_REPO="pref.github.io"
+
+mkdir $DEPLOY_DIR
+cp PREF/PREF $DEPLOY_DIR  # Copy executable
+
+cd $DEPLOY_DIR
+zip -r ../$BUILD_ID.zip * # Generate archive
+cd ..
+
+# Cleanup temporary files
+rm -rf PREF/
+rm -rf PrefLib/
+rm deployed*
+rm Makefile
+rm -rf $DEPLOY_DIR
+
+rm -rf $BUILD_REPO
+git clone -b builds https://${GH_TOKEN}@github.com/PREF/$BUILD_REPO.git
+
+cd $BUILD_REPO
+rm -rf *$OS_NAME*
+mv ../$BUILD_ID.zip .
+
+git config user.email "buildbot@none.io"
+git config user.name "Travis Build Bot"
+git add -A .
+git commit -a -m "Updated Linux Nightly $BUILD_DATE"
+git push --quiet origin builds > /dev/null 2>&1
