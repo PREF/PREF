c4c8f0a8d81dd18544547a41be59fcb684392bc3
Dax89 <trogu.davide@gmail.com>
2017-04-20 10:17:43 +0200

    AppVeyor: Minor fixes

--- ci/deploy.ps1
+++ ci/deploy.ps1
@@ -5,7 +5,7 @@ if(Test-Path $($env:build_repo)) {
     Remove-Item $($env:build_repo) -Recurse -Force 
 }
 
-git clone -b builds https://${$env:github_token}@github.com/PREF/$BUILD_REPO.git > $null 2>&1
+git clone -b builds https://$($env:github_token)@github.com/PREF/$BUILD_REPO.git > $null 2>&1
 cd $(BUILD_REPO)
 Remove-Item *Windows* 
 Move-Item -Path ../$($env:appveyor_build_version).zip -Destination .
