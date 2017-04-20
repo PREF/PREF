c6615b3f6d37cab1c1a3477a7b30e8f683eab72a
Dax89 <trogu.davide@gmail.com>
2017-04-20 11:28:12 +0200

    AppVeyor: Strict checks

--- ci/deploy.ps1
+++ ci/deploy.ps1
@@ -1,3 +1,5 @@
+$archive = $env:appveyor_build_version + ".zip"
+
 git config core.autocrlf true
 git config --global credential.helper store
 Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:github_token):x-oauth-basic@github.com`n"
@@ -9,10 +11,10 @@ if(Test-Path $($env:build_repo)) {
 git clone -b builds https://$($env:github_token)@github.com/PREF/$($env:build_repo).git > $null 2>&1
 cd $($env:build_repo)
 Remove-Item *Windows* 
-Move-Item -Path ../$($env:appveyor_build_version).zip -Destination .
     
-if(Test-Path $($env:appveyor_build_version))
+if(Test-Path ../$archive)
 {
+    Move-Item -Path ../$archive -Destination .
     git config --global user.email "buildbot@none.io"
     git config --global user.name "AppVeyor Build Bot"
     git add -A .
--- ci/package.ps1
+++ ci/package.ps1
@@ -1,7 +1,9 @@
 cd PREF\release
 
+$archive = $env:appveyor_build_version + ".zip"
+
 windeployqt --release PREF.exe
-7z a -tzip (get-item env:appveyor_build_version).zip *
-Move-Item -Path (get-item env:appveyor_build_version).zip -Destination ..\..
+7z a -tzip $archive *
+Move-Item -Path $archive -Destination ..\..
 
 cd ..\..
