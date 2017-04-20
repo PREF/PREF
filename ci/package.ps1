4f72ba99482fc56a6777e2d37d7e920b9bae9afc
Dax89 <trogu.davide@gmail.com>
2017-04-20 11:38:13 +0200

    AppVeyor: Don't copy intermediate file in archive

--- ci/package.ps1
+++ ci/package.ps1
@@ -1,9 +1,15 @@
-cd PREF\release
-
 $archive = $env:appveyor_build_version + ".zip"
 
-windeployqt --release PREF.exe
-7z a -tzip $archive *
-Move-Item -Path $archive -Destination ..\..
+cd PREF\release
+mkdir deploy
 
+Copy-Item PREF.exe deploy
+cd deploy
+windeployqt --release PREF.exe
+
+7z a -tzip $archive *
+Move-Item -Path $archive -Destination ..\..\..
+
+cd ..
+Remove-Item deploy -Recurse -Force 
 cd ..\..
