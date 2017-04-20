f2053ba06496353d826422c71c0b82651e6e0046
Dax89 <trogu.davide@gmail.com>
2017-04-20 10:17:02 +0200

    AppVeyor: Use $env specifier

--- ci/deploy.ps1
+++ ci/deploy.ps1
@@ -5,10 +5,10 @@ if(Test-Path $($env:build_repo)) {
     Remove-Item $($env:build_repo) -Recurse -Force 
 }
 
-git clone -b builds https://${GITHUB_TOKEN}@github.com/PREF/$BUILD_REPO.git > $null 2>&1
+git clone -b builds https://${$env:github_token}@github.com/PREF/$BUILD_REPO.git > $null 2>&1
 cd $(BUILD_REPO)
 Remove-Item *Windows* 
-Move-Item -Path ../$(APPVEYOR_BUILD_VERSION).zip -Destination .
+Move-Item -Path ../$($env:appveyor_build_version).zip -Destination .
 git config --global user.email "buildbot@none.io"
 git config --global user.name "AppVeyor Build Bot"
 git add -A .
