6bd8801e77b99599e7ec97b0432796c44b1fe410
Dax89 <trogu.davide@gmail.com>
2017-04-20 10:59:20 +0200

    AppVeyor: Fixed zip archive path

--- ci/package.ps1
+++ ci/package.ps1
@@ -2,6 +2,6 @@ cd PREF\release
 
 windeployqt --release PREF.exe
 7z a $($env:appveyor_build_version).zip *
-Move-Item -Path ../$($env:appveyor_build_version).zip -Destination ..\..
+Move-Item -Path $($env:appveyor_build_version).zip -Destination ..\..
 
 cd ..\..
